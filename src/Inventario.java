b6a87bec70cd22ba4f5b330ac80f7a222e8f2e67
yassir <yaserr11@alu.edu.gva.es>
2026-02-04 13:29:46 +0100

    Ejercicio de repaso hasta ordernarPorPrecioAsc

--- src/Inventario.java
+++ src/Inventario.java
@@ -1,10 +1,109 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Inventario {
 
     private ArrayList<ProductoInformatico> productos = new ArrayList<>();
 
+    public Inventario() {
+    }
 
+    public Inventario(ArrayList<ProductoInformatico> productos) {
+        this.productos = productos;
+    }
 
+    public void listarTodos() {
+        for (ProductoInformatico p : productos){
+            System.out.println(p.toString());
+        }
+    }
+
+    public void listarSinStock() {
+
+        for (ProductoInformatico p : productos){
+            if (p.getStock() == 0){
+            System.out.println(p.toString());
+            }
+        }
+    }
+
+    public boolean agregarProducto(ProductoInformatico p) {
+        return productos.add(p);
+    }
+
+    @Override
+    public String toString() {
+        return "Inventario{" +
+                "productos=" + productos +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Inventario that = (Inventario) o;
+        return Objects.equals(productos, that.productos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(productos);
+    }
+
+    public List<ProductoInformatico> buscarPorNombre(String nombre) {
+        List<ProductoInformatico> resultados = new ArrayList<>();
+        for (ProductoInformatico p : productos){
+           if (p.getNombre().toLowerCase().contains(nombre.toLowerCase())){
+               resultados.add(p);
+           }
+        }
+        return resultados;
+    }
+
+    public List<ProductoInformatico> buscarPorCategoria(String cat) {
+        List<ProductoInformatico> resultados = new ArrayList<>();
+        for (ProductoInformatico p : productos){
+            if (p.getCategoria().toLowerCase().contains(cat.toLowerCase())){
+                resultados.add(p);
+            }
+        }return resultados;
+    }
+
+    public List<ProductoInformatico> buscarPorPrecio(double min, double max) {
+        List<ProductoInformatico> resultados = new ArrayList<>();
+        for (ProductoInformatico p : productos){
+            if (min <= p.getPrecio() && max >= p.getPrecio()){
+                resultados.add(p);
+            }
+        } return resultados;
+    }
+
+    public boolean incrementarStock(String cod, int c) {
+        boolean incremente = false;
+        if (c >0){
+            for (ProductoInformatico p : productos){
+                if (p.getCodigo().equals(cod)){
+                    p.setStock(p.getStock()+c);
+                    incremente= true;
+                }
+            }
+        } return incremente;
+    }
+
+    public boolean decrementarStock(String cod, int c) {
+        boolean decremente = false;
+        if (c >0){
+            for (ProductoInformatico p : productos){
+                if (p.getCodigo().equals(cod)){
+                    if (p.getStock()>= c) {
+                        p.setStock(p.getStock() - c);
+                        decremente = true;
+                    }
+                }
+            }
+        } return decremente;
+    }
+
+    public List<ProductoInformatico> ordenarPorPrecioAsc() {
+
+    }
 }
--- src/ProductoInformatico.java
+++ src/ProductoInformatico.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class ProductoInformatico {
 
     private String codigo;
@@ -21,4 +23,78 @@ public class ProductoInformatico {
 
 
     // getters y setters...
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        if (stock >0)
+        this.stock = stock;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        if (precio>0)
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductoInformatico{" +
+                "categoria='" + categoria + '\'' +
+                ", codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", stock=" + stock +
+                ", marca='" + marca + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductoInformatico that = (ProductoInformatico) o;
+        return Objects.equals(codigo, that.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(codigo);
+    }
 }
